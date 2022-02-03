9b0f5f306cb08b52bc8986571c6854bba67b2759
reinaldoper83@gmail.com <reinaldoper83@gmail.com>
2022-02-03 17:07:10 -0300

    Create produts

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import home_page, about_page, contact_page, login_page, register_page
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
         path('', home_page),
         path('about/', about_page),
@@ -24,3 +26,6 @@ urlpatterns = [
         path('register/', register_page),
         path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
